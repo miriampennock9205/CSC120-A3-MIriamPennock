af5e3406b4b3ed6a2d10bda4da983526001242ab
Miriam Pennock <mpennock@smith.edu>
2024-09-22 11:18:08 -0400

    First (and second) renditions of code edits and work

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,24 @@
-class Conversation {
+import java.util.Scanner;
 
+class Conversation {
+  /**
+   * The main method where the conversation is started. It handles user input, processes responses, 
+   * and stores the transcript.
+   * @param arguments
+   */
   public static void main(String[] arguments) {
     // You will start the conversation here.
+
+    //Create Scanner to get input from the user
+    Scanner scan = new Scanner(System.in);
+    
+    //Ask for the number of rounds
+    System.out.println("How many rounds?");
+    int rounds = scan.nextInt();
+    scan.nextLine(); //Get the newline after the number input
+
+    //Start the conversation
+    
+
   }
 }
